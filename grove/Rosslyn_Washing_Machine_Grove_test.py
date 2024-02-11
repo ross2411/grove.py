91ca15227083a5cd70a5b8d2101acee25a34f539
Ross Ellerington <ross.ellerington@tredsoft.co.uk>
2024-02-11 11:32:30 +0000

    Added code to check if we have a leak

---
+++ grove/Rosslyn_Washing_Machine_Grove_test.py
@@ -0,0 +1,25 @@
+import time, sys, math
+from grove.grove_water_sensor import GroveWaterSensor
+
+def Average(lst):
+    return sum(lst) /len(lst)
+
+PIN = 2
+sensor = GroveWaterSensor(PIN)
+queue = []
+print('Detecting ...')
+while True:
+    value = sensor.value
+    if sensor.value < 500:
+        print("{}, Detected Water.".format(value))
+    else:
+        print("{}, Dry.".format(value))
+    queue.append(value)
+    if (queue.count > 5):
+        queue.pop(0)
+
+    avg = Average(queue)
+    if (avg < 500):
+        print(f"Major water leak detected!!!!")
+        exit(0)
+    time.sleep(.1)
